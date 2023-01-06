5a5fdef9bb0afe82d2060eff14fbeedcfc89beba
N <zdimend@gmail.com>
2023-01-06 15:05:58 +0330

    Add files via upload

--- AnB/translator.js
+++ AnB/translator.js
@@ -1,51 +1,14 @@
 const operate = (inp, z, i) => {
   if (inp[i] == z) {
     inp[i] = "." + z;
-    t = 0;
-    if (inp[i + 1] != "(") {
-      inp[i + 1] = "(" + inp[i + 1] + "))";
-    }
-    else if (inp[i + 1] == "(") {
-      f = 1;
-      while (inp[t] != ")") {
-        t++;
-      }
-      if (inp[t] == "(") {
-        f++;
-      }
-      else if (inp[t] == ")") {
-        f--;
-        if (f == 0) {
-          inp[t] = inp[t] + ")";
-        }
-      }
-    }
-    else {
-      inp[i - 1] = "(" + inp[i - 1];
-    }
-    if (inp[i - 1] == ")") {
-      f = 1;
-      while (inp[t] != "(") {
-        t--;
-      }
-      if (inp[t] == ")") {
-        f++;
-      }
-      else if (inp[t] == "(") {
-        f--;
-        if (f == 0) {
-          inp[t] = "(" + inp[t];
-        }
-      }
-    }
-    else {
-      inp[i - 1] = "(" + inp[i - 1];
+    if (inp[i+1]!="(") {
+      inp[i+1]="("+inp[i+1]+")";
     }
   }
   return inp;
 }
 const translate = (inp) => {
-  inp = inp.toLowerCase().replaceAll("-", "_").replaceAll("a'", "(a')").replaceAll("b'", "(b')").replaceAll("m'", "(m')").replaceAll("'", "'m").split("");
+  inp = inp.toLowerCase().replaceAll("-", "_").replaceAll("a'", "(a'm)").replaceAll("b'", "(b'm)").replaceAll("m'", "(m'm)").split("");
   i = 0;
   o = "";
   for (z in inp) {
